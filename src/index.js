40cc05b77344f85cde53e4a340b2f78c22dc0caa
shishing <sisakovaivana@gmail.com>
2023-06-15 20:12:01 +0200

    Vykreslenie stránky podľa zadania

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './style.css';
 
 console.log('funguju!');
+
+import { Banner } from './components/Banner';
+import { Contact } from './components/Contact';
+import { Footer } from './components/Footer';
+import { Gallery } from './components/Gallery'; 
+import { Header } from './components/Header';
+import { Menu } from './components/Menu';
+
+
+
+const pageElement = document.createElement('div');
+pageElement.classList.add('page');
+
+const main = document.createElement('main');
+main.append(Banner(), Menu(), Gallery(), Contact());
+
+pageElement.append(Header(), main,  Footer());
+
+document.querySelector('#app').append(pageElement);
