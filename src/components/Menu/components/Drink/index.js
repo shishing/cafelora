d92572c2aaebd5744bca794f4508d39e550a93c0
shishing <sisakovaivana@gmail.com>
2023-06-22 19:49:04 +0200

    objednanie nápoja funkčné

--- src/components/Menu/components/Drink/index.js
+++ src/components/Menu/components/Drink/index.js
@@ -28,50 +28,36 @@ export const Drink = (props) => {
     const drinkInfoELm = element.querySelector(".drink__info")
     drinkInfoELm.append(...layers.map((layer) => Layer(layer)))
 
+
+    const btn = element.querySelector(".order-btn")
+
+    if (ordered) {
+      btn.innerHTML = `Zrušit`
+      btn.classList.add(".order-btn--ordered")
+    } else {
+      btn.innerHTML = `Objednat`
+    }
+
+    btn.addEventListener("click", ()=>{
+      fetch(`https://cafelora.kodim.app/api/me/drinks/${id}`, {
+        method: "PATCH",
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c3IiOiJzaGlzaGluZyIsInNjcCI6ImFwcCIsImlhdCI6MTY4NzQ1MDQyMX0.3oReSsO79o4DX-5KUniH9o5vgmn5fFHUywg2GPoAy9E',
+        },
+        body: JSON.stringify({
+          "ordered": !ordered
+        })
+      })
+      .then((response) => response.json())
+      .then(data => {
+        console.log(data)
+        element.replaceWith(Drink(data.result))
+      })
+
+    })
+
   return element
 
-//   <div class="drink">
-//     <div class="drink__product">
-//       <div class="drink__cup">
-//         <img src="https://cafelora.kodim.app/assets/cups/doppio.png">
-//       </div>
-//       <div class="drink__info">
-//         <h3>Doppio</h3>
-//         <div class="layer">
-//           <div class="layer__color" style="background-color: #613916"></div>
-//           <div class="layer__label">espresso</div>
-//         </div>
-//       </div>
-//     </div>
-//     <div class="drink__controls">
-//       <button class="order-btn">
-//         Objednat
-//       </button>
-//     </div>
-//   </div>
 
-//   <div class="drink">
-//     <div class="drink__product">
-//       <div class="drink__cup">
-//         <img src="https://cafelora.kodim.app/assets/cups/lungo.png">
-//       </div>
-//       <div class="drink__info">
-//         <h3>Lungo</h3>
-//         <div class="layer">
-//           <div class="layer__color" style="background-color: #b0dee1"></div>
-//           <div class="layer__label">voda</div>
-//         </div>
-//         <div class="layer">
-//           <div class="layer__color" style="background-color: #613916"></div>
-//           <div class="layer__label">espresso</div>
-//         </div>
-//       </div>
-//     </div>
-//     <div class="drink__controls">
-//       <button class="order-btn">
-//         Objednat
-//       </button>
-//     </div>
-//   </div>
-// </div> 
 }
